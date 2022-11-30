7c314d6bac1a1f2ad39a8ed754635461dee16e36
annt <annt@gft.com>
2022-11-30 12:24:49 +0000

    First commit passing 2 tests

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,16 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int calculateMinimumHP(int[][] dungeon) {
+
+        int minimumHealth = 1;
+
+        minimumHealth += Math.abs(dungeon[0][0]);
+
+            return minimumHealth;
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,39 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int[][] dungeon;
+    int result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void dungeonWIthOneRowAndOneColumnAndDontLoseOrGainHealth(){
+
+        result = 1;
+        dungeon = new int[][] {{0}};
+
+        assertEquals(result, solution.calculateMinimumHP(dungeon));
+    }
+
+    @Test
+    public void dungeonWIthOneRowAndOneColumnAndLoseHealth(){
+
+        result = 5;
+        dungeon = new int[][] {{-4}};
+
+        assertEquals(result, solution.calculateMinimumHP(dungeon));
+    }
 }
