bcf2c7983cb5b1446fb6a6f779bc0102d7e8ca61
annt <annt@gft.com>
2022-11-30 16:53:50 +0000

    Done with recursion

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -9,31 +9,40 @@ public class Solution {
 
     public int calculateMinimumHP(int[][] dungeon) {
 
-        int m = dungeon.length, n = dungeon[0].length;
-        int[][] next = new int[m+1][n+1];
+        Integer[][] aux = new Integer[dungeon.length][dungeon[0].length];
 
-        for(int i=0; i<=m; i++){
+        return calculate(dungeon, 0, 0, aux);
+    }
 
-            Arrays.fill(next[i], Integer.MAX_VALUE);
-        }
+    private int calculate(int[][] a, int m, int n, Integer[][] aux) {
 
-        next[m-1][n] = 1;
-        next[m][n-1] = 1;
+        if (m == a.length - 1 && n == a[0].length - 1)
 
-        for(int i=m-1; i>=0; i--){
-            for(int j=n-1; j>=0; j--){
+            if (a[m][n] > 0){
 
-                int minH = Math.min(next[i][j+1], next[i+1][j]) - dungeon[i][j];
+                return 1;
+            }else{
 
-                if(minH < 1){
-
-                    minH = 1;
-                }
-
-                next[i][j] = minH;
+                return -a[m][n] + 1;
             }
+
+        if (aux[m][n] != null){
+
+            return aux[m][n];
         }
 
-        return next[0][0];
+        if (m == a.length - 1){
+
+            return aux[m][n] = Math.max(1, calculate(a, m, n + 1, aux) - a[m][n]);
+        }
+
+        if (n == a[0].length - 1){
+
+            return aux[m][n] = Math.max(1, calculate(a, m + 1, n, aux) - a[m][n]);
+        }
+
+
+        return aux[m][n] = Math.max(1, Math.min(calculate(a, m + 1, n, aux) - a[m][n],
+                calculate(a, m, n + 1, aux) - a[m][n]));
     }
 }
